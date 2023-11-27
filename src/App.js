6c8526e30847283e7816a04b24c3642d905de5d0
Arshad Khan <108711604+arshadkh507@users.noreply.github.com>
2023-11-27 14:16:25 +0500

    ready to deploye

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Header } from "./components/Header";
-import  Footer  from "./components/Footer";
-import  TaskList  from "./components/TaskList";
+import Footer from "./components/Footer";
+import TaskList from "./components/TaskList";
 import "./App.css";
 import AddTask from "./components/AddTask";
 import CompletedTasks from "./components/CompletedTasks";
@@ -12,22 +12,43 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
     setTasks(storedTasks);
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    const saveTasksToLocalStorage = () => {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    };
+
+    const debouncedSave = setTimeout(saveTasksToLocalStorage, 500);
+
+    return () => clearTimeout(debouncedSave);
   }, [tasks]);
 
   return (
     <div className="App">
       <Header />
-      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <main>
         <AddTask tasks={tasks} setTasks={setTasks} />
         <TaskList tasks={tasks} setTasks={setTasks} />
-        {tasks.some(task => task.completed) && <CompletedTasks completedTasks={tasks.filter(task => task.completed)} setTasks={setTasks} />}
+        {tasks.some((task) => task.completed) && (
+          <CompletedTasks
+            completedTasks={tasks.filter((task) => task.completed)}
+            setTasks={setTasks}
+          />
+        )}
       </main>
       <Footer />
     </div>
